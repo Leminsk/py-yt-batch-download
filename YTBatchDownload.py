2011cf78f927d7d62d895c55dcd28444d479ce82
Matheus Lemos <79233501+Leminsk@users.noreply.github.com>
2023-07-23 01:21:31 -0300

    download mp3s from youtube urls (in txt)

---
+++ YTBatchDownload.py
@@ -0,0 +1,75 @@
+import youtube_dl
+
+file_name = "links.txt"
+
+error_urls = []
+
+def downloadMP3(video_url):
+    video_info = youtube_dl.YoutubeDL().extract_info(
+        url = video_url,download=False
+    )
+    filename = f"{video_info['title']}.mp3"
+    options={
+        'format':'bestaudio/best',
+        'keepvideo':False,
+        'outtmpl':filename,
+    }
+
+    with youtube_dl.YoutubeDL(options) as ydl:
+        ydl.download([video_info['webpage_url']])
+
+    print("Download complete ---> {}".format(filename))
+
+def isValidURLList(error_url_list):
+    invalid_errors = ['DownloadError']
+
+    for error_url in error_url_list:
+        if(error_url[0] not in invalid_errors):
+            return True
+        
+    return False
+
+# download from "links.txt"
+with open(file_name) as file:
+    for line in file:
+        if(line.startswith("https")):
+            video_url = line.rstrip()
+            
+            try:
+                downloadMP3(video_url)
+
+            except Exception as e:
+                print("--------------------------------------------------")
+                print("Error with URL: ", video_url)
+                print(e)
+                print("--------------------------------------------------")
+                error_urls.append([type(e).__name__, video_url])
+
+
+print("SOME URL's FAILED!")
+print("RETRYING...")
+
+# retry downloading
+while(len(error_urls) > 0 and isValidURLList(error_urls)):
+
+    itemsToDelete = []
+
+    for i in range(len(error_urls)):
+        video_url = error_urls[i][1]
+        try:
+            downloadMP3(video_url)
+            itemsToDelete.append(i)
+        except Exception as e:
+                print("--------------------------------------------------")
+                print("Error with URL: ", video_url)
+                print(e)
+                print("--------------------------------------------------")
+        
+    for i in itemsToDelete:
+        del error_urls[i]
+
+    
+if(len(error_urls) > 0):
+    print("These URL's are troublesome:")
+    for e in error_urls:
+        print(e)
