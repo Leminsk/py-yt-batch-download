b4c5617a3b4da294ce34ccacb04d9124cb828d7a
Matheus Lemos <79233501+Leminsk@users.noreply.github.com>
2023-07-23 01:48:26 -0300

    deal with playlists

--- YTBatchDownload.py
+++ YTBatchDownload.py
@@ -1,24 +1,24 @@
 import youtube_dl
 
 file_name = "links.txt"
+output_folder = "output"
 
 error_urls = []
 
 def downloadMP3(video_url):
     video_info = youtube_dl.YoutubeDL().extract_info(
-        url = video_url,download=False
+        url=video_url, download=False
     )
-    filename = f"{video_info['title']}.mp3"
+    
     options={
-        'format':'bestaudio/best',
-        'keepvideo':False,
-        'outtmpl':filename,
+        'format': 'bestaudio/best',
+        'keepvideo': False,
+        'outtmpl': f"{output_folder}/%(title)s.mp3",
     }
 
     with youtube_dl.YoutubeDL(options) as ydl:
         ydl.download([video_info['webpage_url']])
 
-    print("Download complete ---> {}".format(filename))
 
 def isValidURLList(error_url_list):
     invalid_errors = ['DownloadError']
@@ -45,31 +45,31 @@ with open(file_name) as file:
                 print("--------------------------------------------------")
                 error_urls.append([type(e).__name__, video_url])
 
-
-print("SOME URL's FAILED!")
-print("RETRYING...")
-
-# retry downloading
-while(len(error_urls) > 0 and isValidURLList(error_urls)):
-
-    itemsToDelete = []
-
-    for i in range(len(error_urls)):
-        video_url = error_urls[i][1]
-        try:
-            downloadMP3(video_url)
-            itemsToDelete.append(i)
-        except Exception as e:
-                print("--------------------------------------------------")
-                print("Error with URL: ", video_url)
-                print(e)
-                print("--------------------------------------------------")
-        
-    for i in itemsToDelete:
-        del error_urls[i]
-
-    
 if(len(error_urls) > 0):
-    print("These URL's are troublesome:")
-    for e in error_urls:
-        print(e)
+    print("SOME URL's FAILED!")
+    print("RETRYING...")
+
+    # retry downloading
+    while(len(error_urls) > 0 and isValidURLList(error_urls)):
+
+        itemsToDelete = []
+
+        for i in range(len(error_urls)):
+            video_url = error_urls[i][1]
+            try:
+                downloadMP3(video_url)
+                itemsToDelete.append(i)
+            except Exception as e:
+                    print("--------------------------------------------------")
+                    print("Error with URL: ", video_url)
+                    print(e)
+                    print("--------------------------------------------------")
+            
+        for i in itemsToDelete:
+            del error_urls[i]
+
+        
+    if(len(error_urls) > 0):
+        print("These URL's are troublesome:")
+        for e in error_urls:
+            print(e)
